dc84ea8a67299476c00f5f6102e31272097b4a3a
ValensFlorencia <160854619+ValensFlorencia@users.noreply.github.com>
2024-11-13 17:53:17 +0700

    commit 3

--- candi/lib/main.dart
+++ candi/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:candi/data/candi_data.dart';
+import 'package:candi/screens/detail_screen.dart';
 
 void main() {
   runApp(const MainApp());
@@ -10,20 +12,13 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Wisata Candi",
+      title: 'Wisata Candi',
       theme: ThemeData(
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true),
-      home: DetailScreen(),
+      home: DetailScreen(
+        candi: candiList[0],
+      ),
     );
-  }
-}
-
-class DetailScreen extends StatelessWidget {
-  const DetailScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
   }
 }
